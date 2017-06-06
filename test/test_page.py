b2635b5d6e9662bdac43ac116bcc9f8ab2a68e69
Ken McGrady <ken.mcgrady@gmail.com>
2017-06-05 20:12:25 -0700

    Add more tags

--- test/test_page.py
+++ test/test_page.py
@@ -29,5 +29,20 @@ class TestHTML(unittest.TestCase):
     def test_has_p_tag(self):
         self.assertTrue(self.parser.has_tag('p'), 'Missing a paragraph tag')
 
+    def test_has_header_tag(self):
+        self.assertTrue(self.parser.has_tag('header'), 'Missing a header tag')
+
+    def test_has_footer_tag(self):
+        self.assertTrue(self.parser.has_tag('footer'), 'Missing a footer tag')
+
+    def test_has_main_tag(self):
+        self.assertTrue(self.parser.has_tag('main'), 'Missing a main tag')
+
+    def test_has_article_tag(self):
+        self.assertTrue(self.parser.has_tag('article'), 'Missing a article tag')
+
+    def test_has_img_tag(self):
+        self.assertTrue(self.parser.has_tag('img'), 'Missing a img tag')
+
 if __name__ == '__main__':
     unittest.main()
